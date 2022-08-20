769fddbef92b2eef7c5b947b27a5c990bd81fa1b
KimJinHyeon0 <rlawlsgus117@gmail.com>
2022-08-20 19:18:33 +0900

    [ADD] test data loader [ADD] Discriminator

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ import torchvision.transforms.functional as F
 import os
 import math
 import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -41,11 +43,11 @@ def class_balancing(len_p, l, batch):
             recur = False
     return patch_indices
 
-def load_dataset(mode):
+def load_dataset(data_type):
     paths = []
     forgery_box = []
 
-    if mode == 'train':
+    if data_type == 'train':
         img_ = lambda x: f'./dataset/findit/train/img/{str(x).zfill(3)}.jpg'
         xml_ = lambda x: f'./dataset/findit/train/xml/{str(x).zfill(3)}.xml'
 
@@ -64,36 +66,56 @@ def load_dataset(mode):
 
         assert len(paths) == len(forgery_box)
 
-    # elif mode == 'val':
-    #     img_ = lambda x: f'./dataset/findit/val/img/{x}.jpg'
-    #     xml_path = f'./dataset/findit/val/labels.xml'
-    #
-    #     tree = ET.parse(xml_path)
-    #     root = tree.getroot()
-    #     for f in root:
-    #         id, label = f.attrib.get('id'), f.attrib.get('modified')
-    #         img_path = img_(id)
-    #         paths.append(img_path)
-    #         labels.append(label)
-    #     assert len(paths) == len(labels)
-    #     forgery_box = [0]*len(paths)
+        return np.array(paths), np.array(forgery_box, dtype=object)
 
-    # elif mode == 'test':
-    #     img_ = lambda x: f'./dataset/personal_datasets/img/{x}.jpg'
-    #     xml_path = f'./dataset/personal_datasets/labels.xml'
-    #
-    #     tree = ET.parse(xml_path)
-    #     root = tree.getroot()
-    #     for f in root:
-    #         id, label = f.attrib.get('id'), f.attrib.get('modified')
-    #         img_path = img_(id)
-    #         paths.append(img_path)
-    #         labels.append(label)
-    #     assert len(paths) == len(labels)
+    elif data_type == 'test':
+        path = f'./dataset/personal_datasets'
+        paths = []
+
+        pristine = [os.path.join(path, '0', f) for f in os.listdir(os.path.join(path, '0')) if
+                    os.path.isfile(os.path.join(path, '0', f))]
+        forged = [os.path.join(path, '1', f) for f in os.listdir(os.path.join(path, '1')) if
+                  os.path.isfile(os.path.join(path, '1', f))]
+
+        paths.extend(pristine)
+        paths.extend(forged)
+
+        labels = np.concatenate((np.zeros(len(pristine)), (np.ones(len(forged)))))
+
+        return paths, labels
     else:
         raise ValueError('Check datasets')
 
-    return np.array(paths), np.array(forgery_box, dtype=object)
+def demo_transform(path, patch_size, overlapping_ratio):
+    img = Image.open(path)
+    img = F.to_tensor(img)
+    img.to(device)
+    raw_img = img.clone().detach()
+    (h, w) = img.shape[1:]
+    kernel_size = int(np.lcm(8, patch_size))
+    stride = int(kernel_size * overlapping_ratio)
+
+    h_cut, w_cut = (h // kernel_size) * kernel_size, (w // kernel_size) * kernel_size
+    upper_h_cut, left_w_cut = round((h - h_cut) / 2), round((w - w_cut) / 2)
+
+    img = torch.narrow(img, 1, upper_h_cut, h_cut)
+    img = torch.narrow(img, 2, left_w_cut, w_cut)
+    narrow_img = img.clone().detach()
+    img = img.unsqueeze(0)
+
+    patches = img.unfold(2, kernel_size, int(patch_size * overlapping_ratio)) \
+        .unfold(3, kernel_size, int(kernel_size * overlapping_ratio))
+
+    overlapping_patches = torch.empty((patches.shape[2] * patches.shape[3], 3, kernel_size, kernel_size), device=device)
+    patch_indices_map = np.zeros((overlapping_patches.shape[0], 4), dtype=np.int32)
+    c = 0
+    for i in range(patches.shape[2]):
+        for j in range(patches.shape[3]):
+            overlapping_patches[c] = patches[:, :, i, j, :]
+            patch_indices_map[c] = [i*stride, i*stride+kernel_size, j*stride, j*stride+kernel_size]
+            c += 1
+
+    return raw_img, narrow_img, overlapping_patches, patch_indices_map
 
 
 def transform_img(path, f_box, patch_size, overlapping_ratio):
@@ -173,4 +195,59 @@ class EarlyStopMonitor(object):
             self.is_best = False
 
         return self.num_round >= self.max_round
+
+class Discriminate():
+    def __init__(self, img_shape, visualize, cal_method, th):
+
+        self.prob_map = torch.zeros(img_shape, device=device)
+        self.cnt = torch.zeros(img_shape, device=device)
+        self.visualize = visualize
+        self.method = cal_method
+        self.th = th
+
+        self.mask = None
+        self.output = None
+
+    def fill(self, mask_cut, prob):
+        for (x_s, x_d, y_s, y_d), p in zip(mask_cut, prob):
+            if self.method == 'max':
+                self.prob_map[0][x_s:x_d, y_s:y_d] = \
+                    torch.maximum(self.prob_map[0][x_s:x_d, y_s:y_d],
+                                  torch.ones((x_d-x_s, y_d-y_s), device=device) * p)
+
+            elif self.method == 'mean':
+                self.prob_map[0][x_s:x_d, y_s:y_d] += p
+                self.cnt[0][x_s:x_d, y_s:y_d] += 1
+
+    def score(self):
+        if self.method == 'max':
+            self.output = self.prob_map
+
+        elif self.method == 'mean':
+            self.output = torch.div(self.prob_map, self.cnt)
+
+        self.mask = torch.as_tensor(self.output > self.th, dtype=torch.float32)
+        return self.output, self.mask
+
+def tf(k):
+    return k.permute(1, 2, 0)
+
+def visualize(raw, modi, prob, mask):
+    raw = tf(raw)
+    modi = tf(modi)
+    prob = tf(prob)
+    mask = tf(mask)
+
+    plt.imshow(raw)
+    plt.show()
+    plt.imshow(modi)
+    plt.show()
+
+    fig, axs = plt.subplots(1, 4)
+    a = axs[0].imshow(prob, vmin=0, vmax=1)
+    plt.colorbar(a, ax=axs[1])
+
+    a = axs[2].imshow(mask, vmin=0, vmax=1)
+    plt.colorbar(a, ax=axs[4])
+    plt.show()
 
